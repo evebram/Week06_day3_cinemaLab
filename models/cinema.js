a79410e68f777f28da5a450507490163e82e71ae
= <evebramley@me.com>
2019-07-31 13:10:37 +0100

    list film titles

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,12 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function () {
+  const titleArray = [];
+  this.films.map((item) => {
+    titleArray.push(item.title);
+  })
+  return titleArray;
+};
+
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -24,14 +24,16 @@ describe('Cinema', function () {
   });
 
   it('should have a collection of films', function () {
-    const actual = cinema.films;
-    assert.deepStrictEqual(actual, films);
+    const filmTitles = ['Moonlight', 'Blade Runner 2049', 'Dunkirk', 'Black Panther', 'T2 Trainspotting'];
+    const actual = cinema.filmTitles();
+    assert.deepStrictEqual(actual, filmTitles);
   });
 
   it('should be able to get a list of film titles', function (){
     const actual = cinema.films;
     assert.deepStrictEqual(actual, films);
   });
+
   it('should be able to find a film by title');
   it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
